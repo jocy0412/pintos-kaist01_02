22c64a55a00a89fd198e6c2f23d1680b5d007377
Ubuntu <ubuntu@ip-172-31-37-227.ap-northeast-2.compute.internal>
2022-05-31 03:53:23 +0000

    systemcall halt(), exit(), create() remove()

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -41,6 +41,104 @@ syscall_init (void) {
 void
 syscall_handler (struct intr_frame *f UNUSED) {
 	// TODO: Your implementation goes here.
+	/* ----------- Project2 ----------- */
+	/* 유저 스택에 저장되어 있는 시스템 콜 넘버를 가져오기 */
+	int sys_number = f->R.rax; // rax: 시스템 콜 넘버
+    /*
+	인자 들어오는 순서:
+	1번째 인자 - %rdi
+	2번째 인자 - %rsi
+	3번째 인자 - %rdx
+	4번째 인자 - %r10
+	5번째 인자 - %r8
+	6번째 인자 - %r9
+	*/
+	// TODO: Your implementation goes here.
+	switch(sys_number) {
+		case SYS_HALT:
+			halt();
+		case SYS_EXIT:
+			exit(f->R.rdi);
+		case SYS_FORK:
+			fork(f->R.rdi);
+		case SYS_EXEC:
+			exec(f->R.rdi);
+		case SYS_WAIT:
+			wait(f->R.rdi);
+		case SYS_CREATE:
+			create(f->R.rdi, f->R.rsi);
+		case SYS_REMOVE:
+			remove(f->R.rdi);
+		case SYS_OPEN:
+			open(f->R.rdi);
+		case SYS_FILESIZE:
+			filesize(f->R.rdi);
+		case SYS_READ:
+			read(f->R.rdi, f->R.rsi, f->R.rdx);
+		case SYS_WRITE:
+			write(f->R.rdi, f->R.rsi, f->R.rdx);
+		case SYS_SEEK:
+			seek(f->R.rdi, f->R.rdx);
+		case SYS_TELL:
+			tell(f->R.rdi);
+		case SYS_CLOSE:
+			close(f->R.rdi);
+	}
+	/* ----------- Project2 ----------- */
+
 	printf ("system call!\n");
 	thread_exit ();
 }
+
+/* ----------- Project2 ----------- */
+void check_address(void *addr) {
+	struct thread *curr = thread_current();
+	/* --- Project 2: User memory access --- */
+	// if (!is_user_vaddr(addr)||addr == NULL) 이렇게 사용 했었는데
+	// 이 경우는 유저 주소 영역 내에서도 할당되지 않는 공간 가리키는 것을 체크하지 않음.
+	// 그래서 pml4_get_page를 추가해줘야!
+	if (!is_user_vaddr(addr)||addr == NULL||
+	pml4_get_page(curr->pml4, addr)== NULL)
+	{
+		exit(-1);
+	}
+}
+
+/* pintos 종료하는 시스템 콜 */
+void halt(void){
+	power_off();
+}
+
+/* 현재 프로세스를 종료시키는 시스템 콜 */
+void exit(int status)
+{
+	struct thread *curr = thread_current();
+	printf("%s: exit%d\n", curr->name, status); // Process Termination Message
+	/* 정상적으로 종료됐다면 status는 0 */
+	/* status: 프로그램이 정상적으로 종료됐는지 확인 */
+	thread_exit();
+}
+
+/* 파일 생성하는 시스템 콜 */
+bool create (const char *file, unsigned initial_size) {
+	/* 성공이면 true, 실패면 false */
+	check_address(file);
+	if (filesys_create(file, initial_size)) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+/* 파일 제거하는 시스템 콜 */
+bool remove (const char *file) {
+	check_address(file);
+	if (filesys_remove(file)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+/* ----------- Project2 ----------- */
